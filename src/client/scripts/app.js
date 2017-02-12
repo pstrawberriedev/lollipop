aeab3cca3dad830d9e0872cb6a1c6d4c218d6dc1
JD <pstrawberrie@gmail.com>
2017-02-12 01:12:57 -0700

    recent games almost done, lookin gr8 m8

--- src/client/scripts/app.js
+++ src/client/scripts/app.js
@@ -2,6 +2,7 @@ import $ from 'jquery';
 import TweenMax from 'gsap';
 import axios from 'axios';
 import _ from 'lodash';
+import moment from 'moment';
 const LS = localStorage;
 const apiRoot = 'http://localhost:3000'
 import champions from '../data/champions.json';
@@ -24,16 +25,19 @@ let app = {
     app.currentSummoner = {};
     app.extraMatches = [];
 
-    app.$nav = $('#search');
+    app.$nav = $('nav#nav');
+    app.$navIcons = $('nav#nav .nav-icon');
     app.$devButton = $('#dev-button');
     app.$devMenu = $('#dev-menu');
     app.$search = $('#search');
     app.$searchBar = $('#search-wrap');
     app.$searchTitle = $('#search #title');
-    app.$summonerArea = $('#summoner');
-    app.$summonerInfo = $('#summoner .info');
-    app.$summonerMatches = $('#summoner .matches');
-    app.$summonerRecent = $('#summoner .recent');
+    app.$summonerInfo = $('#summoner-info');
+    app.$summonerInfoWrap = $('#info-wrap');
+    app.$summonerMatches = $('#matches');
+    app.$summonerMatchesWrap = $('#matches-wrap');
+    app.$summonerRecent = $('#recent-games');
+    app.$summonerRecentWrap = $('#recent-wrap');
 
     console.log('%c Lollipop App Started ', 'background: #ddd; color: #9b1851;font-weight:bold;');
     this.syncLocalData();
@@ -115,7 +119,7 @@ let app = {
     // Nav Debug Menu
     self.$devButton.off('click').on('click', function() {
       if(!self.$devMenu.hasClass('on')) {
-        TweenMax.to(devMenu, .25, { height:'65px', autoAlpha:1, ease:Sine.easeInOut });
+        TweenMax.to(devMenu, .25, { height:'95px', autoAlpha:1, ease:Sine.easeInOut });
         self.$devMenu.addClass('on');
       } else {
         TweenMax.to(devMenu, .25, { height:0, autoAlpha:0, ease:Sine.easeInOut });
@@ -166,8 +170,14 @@ let app = {
     const self = this;
     TweenMax.to(self.$searchTitle, .2, { y:'50px', autoAlpha:0, ease:Sine.easeInOut });
     TweenMax.to(self.$search, .35, { padding:0, height:0, ease:Sine.easeInOut });
-    TweenMax.to(self.$searchBar, .35, { y:'-50px', autoAlpha:0, ease:Sine.easeInOut, onComplete:cloneSearch });
+    TweenMax.to(self.$searchBar, .35, { y:'-50px', marginRight:10, autoAlpha:0, ease:Sine.easeInOut, onComplete:cloneSearch });
+    TweenMax.to(self.$nav, .35, { backgroundColor:'#FF3D7F', ease:Sine.easeInOut });
+    TweenMax.to(self.$navIcons, .35, { color:'#f0f0f0', ease:Sine.easeInOut });
+    self.$devMenu.addClass('swap');
+    TweenMax.to($('body'), .35, { backgroundColor:'#f0f0f0', ease:Sine.easeInOut });
+    TweenMax.to($('#nav-main .link'), .35, { color:'#f0f0f0', ease:Sine.easeInOut});
     function cloneSearch() {
+      TweenMax.to($('#nav-main .left .search'), .35, { width:250, autoAlpha:1, ease:Sine.easeInOut});
       self.$searchBar.detach().appendTo($('#nav-main .left .search'));
       $('#logo').fadeIn();
       TweenMax.to($('#logo'), .35, { x:0, autoAlpha:1, ease:Sine.easeInOut});
@@ -270,21 +280,30 @@ let app = {
   // Display Current Summoner
   displaySummoner() {
     const self = this;
-    self.$summonerArea.show();
-    self.$summonerInfo.html('');
+    self.$summonerInfoWrap.html('');
+    function wrapUp() {
+      TweenMax.set(self.$summonerInfoWrap, {height:"auto"})
+      TweenMax.from(self.$summonerInfoWrap, 0.35, {height:0})
+    }
+    TweenMax.to(self.$summonerInfo, .35, { padding:'2rem .6rem', ease:Sine.easeInOut,onComplete:wrapUp });
+
+    // Profile Icon
+    var profileIcon = '';
+    if(self.currentSummoner.profileIconId != '-1') {
+      profileIcon = '<img src="https://ddragon.leagueoflegends.com/cdn/7.2.1/img/profileicon/' + self.currentSummoner.profileIconId + '.png" />'
+    }
 
     // Name
-    if(self.currentSummoner.name) {
-      self.$summonerInfo.append(
-        '<h3 class="name">' + self.currentSummoner.name + '</h3>'
-      );
-    }
-    // Profile Icon
-    if(self.currentSummoner.profileIconId && self.currentSummoner.profileIconId != '-1') {
-      self.$summonerInfo.append(
-        '<img class="icon" src="https://ddragon.leagueoflegends.com/cdn/7.2.1/img/profileicon/' + self.currentSummoner.profileIconId + '.png" />'
-      );
-    }
+    self.$summonerInfoWrap.append(
+      '<div class="inline-block">' +
+        '<div class="icon">' + profileIcon + '</div>' +
+        '<div class="vertical" id="info-basic">' +
+          '<div class="name">' + self.currentSummoner.name + '</div>' +
+          '<div class="level small-body">Level ' + self.currentSummoner.summonerLevel + '</div>' +
+        '</div>' +
+      '</div>'
+    );
+
   },
 
   // Get Current Summoner Matches
@@ -374,18 +393,83 @@ let app = {
   // Display Current Summoner Recent Games
   displaySummonerRecentGames() {
     const self = this;
-    self.$summonerRecent.html('');
-    self.$summonerRecent.append('<h3>Recent Games</h3>');
+    let uid = 1;
+    self.$summonerRecentWrap.html('');
+    self.$summonerRecentWrap.append('<h2 class="recent">Recent Games</h2>');
 
     // Recent Games loop
     for(let value of self.currentSummoner.recent) {
+      let gameType = function() {
+        if(value.gameMode === 'ARAM') {return 'ARAM';}
+        else if(value.gameMode === 'CLASSIC' && value.subType === 'NORMAL') {return 'Normal'}
+        else if(value.gameMode === 'CLASSIC' && value.subType.includes('RANKED')) {return 'Ranked'}
+        else if(value.subType.includes('BOT')) {return 'AI'}
+        else {return '';}
+      }
+      let minionKills = function() {
+        var neutral = 0;
+        var lane = 0;
+        if(value.stats.neutralMinionsKilled != undefined || 0) {
+          neutral = value.stats.neutralMinionsKilled;
+        }
+        if(value.stats.minionsKilled != undefined || 0) {
+          lane = value.stats.minionsKilled;
+        }
+        return neutral + lane;
+      }
+      let itemHtml = function() {
+        var emptyItem = '<div class="item"><img src="images/empty_item.png" /></div>';
+        var itemStart = '<div class="item"><img src="http://ddragon.leagueoflegends.com/cdn/7.3.1/img/item/';
+        var itemEnd = '.png" /></div>';
+        var item0 = value.stats.item0 ? itemStart + value.stats.item0 + itemEnd : emptyItem;
+        var item1 = value.stats.item1 ? itemStart + value.stats.item1 + itemEnd : emptyItem;
+        var item2 = value.stats.item2 ? itemStart + value.stats.item2 + itemEnd : emptyItem;
+        var item3 = value.stats.item3 ? itemStart + value.stats.item3 + itemEnd : emptyItem;
+        var item4 = value.stats.item4 ? itemStart + value.stats.item4 + itemEnd : emptyItem;
+        var item5 = value.stats.item5 ? itemStart + value.stats.item5 + itemEnd : emptyItem;
+        var item6 = value.stats.item6 ? itemStart + value.stats.item6 + itemEnd : emptyItem;
+        return '<div class="items"><div class="left"><div class="top">' + item0 + item1 + item2 + '</div><div class="bottom">' + item3 + item4 + item5 + '</div></div><div class="right">' + item6 + '</div></div>';
+      }
       let win = value.stats.win ? 'win' : 'loss';
-      self.$summonerRecent.append(
-      '<div class="game ' + win +'">' + // open
-      '<img class="icon" src="http://ddragon.leagueoflegends.com/cdn/7.3.1/img/champion/' + self.championIdToName(value.championId) + '.png" />' + //hero portrait
+      let kills = value.stats.championsKilled == undefined ? '<span class="kills">0</span>' : '<span class="kills">' + value.stats.championsKilled + '</span>';
+      let deaths = value.stats.numDeaths == undefined ? '<span class="deaths">0</span>' : '<span class="deaths">' + value.stats.numDeaths + '</span>';
+      let assists = value.stats.assists == undefined ? '<span class="assists">0</span>' : '<span class="assists">' + value.stats.assists + '</span>';;
+      let perfect = value.stats.numDeaths == undefined ? '<div class="award small-body smaller"><span class="icon-star"></span> 0 deaths</div>' : '';
+      let gameDate = moment(value.createDate);
+      let fromDate = gameDate.fromNow();
+      gameDate = gameDate.format('MMM D hh:mm A');
+      let timePlayed = Math.floor(value.stats.timePlayed / 60);
+      let winLoss = value.stats.win == true ? 'win' : 'loss';
+      let winLossEle = value.stats.win == true ? 'win <span class="icon-thumbs-up"></span>' : 'loss <span class="icon-thumbs-down"></span>';
+      let doubleKills = value.stats.doubleKills ? '<div class="award small-body tiny"><span class="icon-star"></span> Double x' + value.stats.doubleKills + '</div>' : '';
+      let tripleKills = value.stats.tripleKills ? '<div class="award small-body tiny"><span class="icon-star"></span> Triple x' + value.stats.tripleKills + '</div>' : '';
+      let quadraKills = value.stats.quadraKills ? '<div class="award small-body tiny"><span class="icon-star"></span> Quadra x' + value.stats.quadraKills + '</div>' : '';
+      let pentaKills = value.stats.pentaKills ? '<div class="award small-body tiny"><span class="icon-star"></span> Penta x' + value.stats.pentaKills + '</div>' : '';
+      let wards = value.stats.wardPlaced && value.stats.wardPlaced != undefined ? '<div class="award small-body tiny"><span class="icon-eye"></span> Warded x' + value.stats.wardPlaced + '</div>' : '';
 
-      '</div>' // close
+      self.$summonerRecentWrap.append(
+      '<div class="game">' +
+        '<div class="vertical">' +
+          '<img class="icon" src="http://ddragon.leagueoflegends.com/cdn/7.3.1/img/champion/' + self.championIdToName(value.championId) + '.png" />' + //hero portrait
+          '<div class="small-body tiny text-center">' + gameType() + '</div>' +
+        '</div>' +
+        '<div class="vertical">' +
+          '<div class="wl ' + winLoss + ' small-body tiny caps">' + winLossEle + '</div>' + //win loss
+          perfect + doubleKills + tripleKills + quadraKills + pentaKills + wards +
+          '<div class="small-body smaller">' + timePlayed + ' min.</div>' + //game duration
+        '</div>' +
+        '<div class="vertical right">' +
+          '<div class="kda text-bold text-right">' + kills + '/' + deaths + '/' + assists + '</div>' + //kda
+          '<div class="cs small-body tiny text-right"><span>' + minionKills() + '</span><img src="images/cs.png" /></div>' + //cs
+          '<div class="gold small-body tiny text-right"><span>' + value.stats.goldEarned.toLocaleString() + '</span><img src="images/gold.png" /></div>' + //gold
+          '<div class="small-body tiny text-right">' + fromDate + '</div>' + //time played from now ("2 hours ago")
+        '</div>' +
+        '<div class="vertical right">' +
+          itemHtml() +
+        '</div>' +
+      '</div>'
       );
+
     }
   }
 
